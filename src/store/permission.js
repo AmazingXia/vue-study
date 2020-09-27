1f854790e71898c4208d486470b928c37a08f394
niumkiki0912 <chuxiaimpala@163.com>
2020-09-27 12:03:35 +0800

    格式化代码

--- src/store/permission.js
+++ src/store/permission.js
@@ -1,4 +1,7 @@
-import { asyncRoutes, constRoutes } from "@/router";
+import {
+  asyncRoutes,
+  constRoutes
+} from "@/router";
 
 const state = {
   routes: [], // 完整路由表
@@ -14,7 +17,9 @@ const mutations = {
 
 const actions = {
   // 路由生成：在得到用户角色后会第一时间调用
-  generateRoutes({ commit }, roles) {
+  generateRoutes({
+    commit
+  }, roles) {
     return new Promise(resolve => {
       // 根据角色做过滤处理
       const accessedRoutes = filterAsyncRoutes(asyncRoutes, roles);
@@ -34,7 +39,9 @@ export function filterAsyncRoutes(routes, roles) {
 
   routes.forEach(route => {
     // 复制一份
-    const tmp = { ...route };
+    const tmp = {
+      ...route
+    };
     // 如果用户有访问权则加入结果路由表
     if (hasPermission(roles, tmp)) {
       // 如果存在子路由则递归过滤之
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,11 +1,11 @@
 <template>
-  <div>
-    <h2>用户登录</h2>
-    <div>
-      <input type="text" v-model="username" />
-      <button @click="login">登录</button>
-    </div>
-  </div>
+	<div>
+		<h2>用户登录</h2>
+		<div>
+			<input type="text" v-model="username" />
+			<button @click="login">登录</button>
+		</div>
+	</div>
 </template>
 <script>
 export default {
